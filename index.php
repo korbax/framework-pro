87b96989539078bc9fc16a09856007c3f2d4a524
Aleksandr Drobot <drobot76@gmail.com>
2016-12-12 08:25:32 +0200

    init project

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+// framework/index.php
+ini_set('display_errors', 1);
+
+//require_once __DIR__.'/autoload.php';
+//
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//
+//$request = Request::createFromGlobals();
+//
+//$input = $request->get('name', 'World');
+//
+//$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+//
+//$response->send();
+
+
+//require_once __DIR__.'/vendor/autoload.php';
+//
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+//
+//$request = Request::createFromGlobals();
+//
+//$input = $request->get('name', 'World');
+//
+//$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+//
+//$response->setContent('Hello world!aa');
+//
+//$response->send();
+
